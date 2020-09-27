fc1c978e781dc9c63820757ffe4e544d8b25415b
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-09-27 16:11:04 -0600

    added linkin between general -> event -> eventdetailed

--- fric/src/App.js
+++ fric/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import GeneralView from './generalView/generalView';
 import EventContentView from './eventcontentview/eventContentView';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
       <GeneralView />
-      <Router>
-        <Switch>
-        </Switch>
-      </Router>
     </div>
   );
 }
--- fric/src/index.js
+++ fric/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import eventContentView from './eventcontentview/eventContentView';
+import eventDetailedView from './eventcontentview/eventDetailedView';
+
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Route exact path="/">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+   </Route>
+    <Route exact path="/Event" component={eventContentView} />
+    <Route exact path="/EventDetailed" component={eventDetailedView} />
+  </Router>,
   document.getElementById('root')
+
 );
 
 // If you want your app to work offline and load faster, you can change
