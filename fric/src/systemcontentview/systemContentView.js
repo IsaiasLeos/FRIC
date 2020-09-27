140d52ae7c5caf8b2a055d8678db40eb64738d3a
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-09-27 17:02:24 -0600

    added navbar to both + small bugfix

--- fric/src/systemcontentview/systemContentView.js
+++ fric/src/systemcontentview/systemContentView.js
@@ -1,27 +1,34 @@
 import * as React from 'react'
 
+import Table from 'react-bootstrap/Table'
+import '../assets/css/bootstrap.css'
+import AddImage from '../assets/add.png'
+import SortImage from '../assets/updownarrow.png'
+import GeneralView from '../generalView/generalView'
+
 class systemContentView extends React.Component {
     render() {
         return (
             <div>
+                <GeneralView />
                 <div class="SystemContentView">
                     <div id="systemTable">
                         <h2>System Table</h2>
 
-                        <table>
+                        <Table>
                             <tr>
                                 <th>Select</th>
                                 <th>System
-                        <input type="image" src="../assets/updownarrow.png" />
+                        <input type="image" src={SortImage} />
                                 </th>
                                 <th>No. of Task
-                        <input type="image" src="../assets/updownarrow.png" />
+                        <input type="image" src={SortImage} />
                                 </th>
                                 <th>No. Findings
-                        <input type="image" src="../assets/updownarrow.png" />
+                        <input type="image" src={SortImage} />
                                 </th>
                                 <th>Progress
-                        <input type="image" src="../assets/updownarrow.png" />
+                        <input type="image" src={SortImage} />
                                 </th>
                             </tr>
                             <tr>
@@ -52,63 +59,63 @@ class systemContentView extends React.Component {
                                 <td>20</td>
                                 <td>Completeed</td>
                             </tr>
-                        </table>
+                        </Table>
                     </div>
                 </div>
                 <div class="systemDetailedTable" id="systemDetailedTable">
                     <h2>System Detailed View</h2>
                     <h3>System Information</h3>
-                </div>
-                <div class="systemDetailedTextLabels">
-                    <form action="">
-                        <label for="systemName">System Information</label>
-                        <input type="text" id="systemName" name="systemName"></input><br></br>
+                    <div class="systemDetailedTextLabels">
+                        <form action="">
+                            <label for="systemName">System Information</label>
+                            <input type="text" id="systemName" name="systemName"></input><br></br>
 
-                        <label for="systemDescription">System Description</label>
-                        <input type="text" id="systemDescription" name="systemDescription"></input><br></br>
+                            <label for="systemDescription">System Description</label>
+                            <input type="text" id="systemDescription" name="systemDescription"></input><br></br>
 
-                        <label for="systemLocation">System Location</label>
-                        <input type="text" id="systemLocation" name="systemLocation"></input><br></br>
+                            <label for="systemLocation">System Location</label>
+                            <input type="text" id="systemLocation" name="systemLocation"></input><br></br>
 
-                        <label for="systemRouter">System Router</label>
-                        <input type="text" id="systemRouter" name="systemRouter"></input><br></br>
+                            <label for="systemRouter">System Router</label>
+                            <input type="text" id="systemRouter" name="systemRouter"></input><br></br>
 
-                        <label for="systemSwitch">System Switch</label>
-                        <input type="text" id="systemSwitch" name="systemSwitch"></input><br></br>
+                            <label for="systemSwitch">System Switch</label>
+                            <input type="text" id="systemSwitch" name="systemSwitch"></input><br></br>
 
-                        <label for="systemRooom">System Room</label>
-                        <input type="text" id="systemRooom" name="systemRooom"></input><br></br>
+                            <label for="systemRooom">System Room</label>
+                            <input type="text" id="systemRooom" name="systemRooom"></input><br></br>
 
-                        <label for="testPlan">Test Plan</label>
-                        <input type="text" id="testPlan" name="testPlan"></input><br></br>
+                            <label for="testPlan">Test Plan</label>
+                            <input type="text" id="testPlan" name="testPlan"></input><br></br>
 
-                        <input type="submit" value="Submit"></input>
-                    </form>
-                    <h3>System Categorization</h3>
-                    <div class="dropdown">
-                        <button class="dropbtn">Confidentiality</button>
-                        <div class="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                            <input type="submit" value="Submit"></input>
+                        </form>
+                        <h3>System Categorization</h3>
+                        <div class="dropdown">
+                            <button class="dropbtn">Confidentiality</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="dropdown">
-                        <button class="dropbtn">Integrity</button>
-                        <div class="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Integrity</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="dropdown">
-                        <button class="dropbtn">Availability</button>
-                        <div class="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Availability</button>
+                            <div class="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
                         </div>
                     </div>
                 </div>
