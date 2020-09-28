530de1680c9ab74bb98bfbf8ce746f0d2e68d10a
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-09-28 16:41:27 -0600

    added generalview to config

--- fric/src/configurationcontentview/configurationContentView.js
+++ fric/src/configurationcontentview/configurationContentView.js
@@ -1,14 +1,19 @@
 import * as React from 'react'
+import Table from 'react-bootstrap/Table'
+import GeneralView from '../generalView/generalView';
 
 class configurationContentView extends React.Component {
+    
     render() {
         return (
-            <body>
-                
-            </body>
+            <div>
+                <GeneralView/>
+
+               
+
+            </div>
         );
     }
 }
-
 
 export default configurationContentView;
