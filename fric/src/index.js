cd499476e9cefb7409273947c08644213959e0b2
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-09-28 17:09:00 -0600

    fixed imports

--- fric/src/index.js
+++ fric/src/index.js
@@ -7,9 +7,13 @@ import eventContentView from './eventcontentview/eventContentView';
 import eventDetailedView from './eventcontentview/eventDetailedView';
 import analystSummaryView from './analystsummaryview/analystSummaryView';
 import systemContentView from './systemcontentview/systemContentView';
-
+import findingContentView from './findingscontentview/findingContentView';
+import subtaskContentView from './subtaskContentView/subtaskContentView';
+import archiveContentView from './archivecontentview/archiveContentView';
+import configurationContentView from './configurationContentView/configurationContentView';
 import taskContentView from './taskcontentview/taskContentView.js';
-import configurationContentView from './configurationContentView/configurationContentView.js';
+import taskDetailedView from './taskcontentview/taskContentView'
+import subtaskDetailedView from './subtaskContentView/subtaskContentView'
 import setupContentView from './setupContentView/setupContentView.js';
 import helpView from './helpView/helpView.js';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
@@ -20,20 +24,21 @@ ReactDOM.render(
       <React.StrictMode>
         <App />
       </React.StrictMode>,
-   </Route>
+    </Route>
     <Route exact path="/Event" component={eventContentView} />
-    <Route exact path="/EventDetailed" component={eventDetailedView} />
-
+    <Route exact path="/EventDetailed" componsent={eventDetailedView} />
     <Route exact path="/Task" component={taskContentView} />
-
+    <Route exact path="/TaskDetails" component={taskDetailedView} />
+    <Route exact path="/Subtask" component={subtaskContentView} />
+    <Route exact path="/SubtaskDetails" component={subtaskDetailedView} />
     <Route exact path="/AnalystSummary" component={analystSummaryView} />
     <Route exact path="/SystemView" component={systemContentView} />
-    <Route exact path="/ConfigView" component={configurationContentView} />
-    <Route exact path="/SetupView" component={setupContentView} />
-    <Route exact path="/help" component={helpView} />
-    
-
-
+    <Route exact path="/SystemDetailed" component={systemContentView} />
+    <Route exact path="/Findings" component={findingContentView} />
+    <Route exact path="/Archive" component={archiveContentView} />
+    <Route exact path="/Config" component={configurationContentView} />
+    <Route exact path="/Setup" component={setupContentView} />
+    <Route exact path="/Help" component={helpView} />
   </Router>,
   document.getElementById('root')
 
