030efed35a7b94dfde431979f90a2f37576c912f
LXvsqz <avasquez31@miners.utep.edu>
2020-09-29 13:37:22 -0600

    Added table format to findings table

--- fric/src/findingscontentview/findingContentView.js
+++ fric/src/findingscontentview/findingContentView.js
@@ -17,92 +17,81 @@ function TaskContentView(){
     return(
             <div>
                 <GeneralView />
-                <h2>Findings Overview Table</h2>
-                <Table>
-                <tr>
-                                <th>Select</th>
-                                <th>ID
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Title
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>System
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Task
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Subtask
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Analyst
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Classification
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Type
-                        <input type="image" src={SortImage} />
-                                </th>
-                                <th>Risk
-                        <input type="image" src={SortImage} />
-                                </th>
-                            </tr>
 
-                            <tr>
-                                <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
-                                <td>234</td>
-                                <td>Title 1</td>
-                                <td>System W</td>
-                                <td>Task 1 and Task 2</td>
-                                <td>N/A</td>
-                                <td>Alex Vasquez</td>
-                                <td>Open</td>
-                                <td>Vulnerability</td>
-                                <td>Physical Security</td>
-                                <td>VL</td>
-                            </tr>
-                            <tr>
-                                <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
-                                <td>234</td>
-                                <td>Title 1</td>
-                                <td>System W</td>
-                                <td>Task 1 and Task 2</td>
-                                <td>N/A</td>
-                                <td>Alex Vasquez</td>
-                                <td>Open</td>
-                                <td>Vulnerability</td>
-                                <td>Physical Security</td>
-                                <td>VL</td>
-                            </tr>
-                            <tr>
-                                <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
-                                <td>234</td>
-                                <td>Title 1</td>
-                                <td>System W</td>
-                                <td>Task 1 and Task 2</td>
-                                <td>N/A</td>
-                                <td>Alex Vasquez</td>
-                                <td>Open</td>
-                                <td>Vulnerability</td>
-                                <td>Physical Security</td>
-                                <td>VL</td>
-                            </tr>
-                            <tr>
-                                <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
-                                <td>234</td>
-                                <td>Title 1</td>
-                                <td>System W</td>
-                                <td>Task 1 and Task 2</td>
-                                <td>N/A</td>
-                                <td>Alex Vasquez</td>
-                                <td>Open</td>
-                                <td>Vulnerability</td>
-                                <td>Physical Security</td>
-                                <td>VL</td>
-                            </tr>
-                </Table>
+                <br></br>
+                <h2>Findings Overview Table</h2>
+                
+                <table class = "table table-hover table-bordered">
+                    <thead class = "thead-grey">
+                        <tr>
+                            <th>Select</th>
+                            <th>ID<input type="image" src={SortImage} /></th>
+                            <th>Title<input type="image" src={SortImage} /></th>
+                            <th>System<input type="image" src={SortImage} /></th>
+                            <th>Task<input type="image" src={SortImage} /></th>
+                            <th>Subtask<input type="image" src={SortImage} /></th>
+                            <th>Analyst<input type="image" src={SortImage} /></th>
+                            <th>Status<input type="image" src={SortImage} /></th>
+                            <th>Classification<input type="image" src={SortImage} /></th>
+                            <th>Type<input type="image" src={SortImage} /></th>
+                            <th>Risk<input type="image" src={SortImage} /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
+                            <td>234</td>
+                            <td>Title 1</td>
+                            <td>System W</td>
+                            <td>Task 1 and Task 2</td>
+                            <td>N/A</td>
+                            <td>Alex Vasquez</td>
+                            <td>Open</td>
+                            <td>Vulnerability</td>
+                            <td>Physical Security</td>
+                            <td>VL</td>
+                        </tr>
+                        <tr>
+                            <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
+                            <td>234</td>
+                            <td>Title 1</td>
+                            <td>System W</td>
+                            <td>Task 1 and Task 2</td>
+                            <td>N/A</td>
+                            <td>Alex Vasquez</td>
+                            <td>Open</td>
+                            <td>Vulnerability</td>
+                            <td>Physical Security</td>
+                            <td>VL</td>
+                        </tr>
+                        <tr>
+                            <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
+                            <td>234</td>
+                            <td>Title 1</td>
+                            <td>System W</td>
+                            <td>Task 1 and Task 2</td>
+                            <td>N/A</td>
+                            <td>Alex Vasquez</td>
+                            <td>Open</td>
+                            <td>Vulnerability</td>
+                            <td>Physical Security</td>
+                            <td>VL</td>
+                        </tr>
+                        <tr>
+                            <td class="column1"><input type="checkbox" id="" name="" value="1"></input></td>
+                            <td>234</td>
+                            <td>Title 1</td>
+                            <td>System W</td>
+                            <td>Task 1 and Task 2</td>
+                            <td>N/A</td>
+                            <td>Alex Vasquez</td>
+                            <td>Open</td>
+                            <td>Vulnerability</td>
+                            <td>Physical Security</td>
+                            <td>VL</td>
+                        </tr>
+                    </tbody>
+                </table>
 
                 <div id="buttonSet" class="reportButtons">
 
@@ -116,6 +105,7 @@ function TaskContentView(){
                 <br></br>
 
                 <Button variant="outline-dark" onClick={handleShow}>Create Finding</Button>
+
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
                     <Modal.Title>
