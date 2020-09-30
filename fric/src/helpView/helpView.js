61b8614b60a7841b283c884f042ac850bb45c64f
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-09-29 20:50:31 -0600

    updating README

--- fric/src/helpView/helpView.js
+++ fric/src/helpView/helpView.js
@@ -4,20 +4,47 @@ import GeneralView from '../generalView/generalView';
 import 'react-bootstrap'
 import '../assets/css/bootstrap.css'
 class helpView extends React.Component {
-    
+
     render() {
         return (
             <div>
-                <GeneralView/>
-                <h2>Welcome to FRIC help page </h2>
-                <p>
-                    1.0 Download master branch zipped sourcecode <br/>
-                    1.1 Clone the master branch from IsaiasLeo/FRIC repository <br/>
-                    2.0 Install node.js for all libarary dependencies <br/>
-                    3.0 From terminal run : install npm  <br/>
-                    3.1 From terminal run : npm start   <br/>
-                </p>
-                <br/><br/>
+                <GeneralView />
+                <h1 align="center">Welcome to F.R.I.C</h1>
+
+                <blockquote>
+                    <p>The Cyber Experimentation &amp; Analysis Division (CEAD) recognizes the complexity and the time it takes to
+                    manage task assignments, progress, vulnerability discovery during a cyber engagement and generate custom
+                    reports that presents the discovered vulnerabilities and potential issues to CEAD’s target audience. They want a
+                    system that would aid the management of task, collection of evidence, and report generation during a cyber
+                    engagement.
+                    The University of Texas at El Paso (UTEP) and CEAD are collaborating to develop Findings and Reporting
+                    Information Console (FRIC) system that will provide the ability to manage task assignment and progress, and
+facilitate the collection of evidence on existing vulnerabilities, and generation of custom reports.</p>
+                </blockquote>
+                <h3 id="-homepage-https-github-com-isaiasleos-fric-">🏠 <a href="https://github.com/IsaiasLeos/FRIC">Homepage</a></h3>
+                <h2 id="install">Install</h2>
+                <pre><code class="lang-sh">npm <span class="hljs-keyword">install</span>
+                </code></pre>
+                <h2 id="usage">Usage</h2>
+                <pre><code class="lang-sh">npm <span class="hljs-keyword">run</span><span class="bash"> start</span>
+                </code></pre>
+                <h2 id="run-tests">Run tests</h2>
+                <pre><code class="lang-sh">npm <span class="hljs-keyword">run</span><span class="bash"> <span class="hljs-built_in">test</span></span>
+                </code></pre>
+                <h2 id="author">Author</h2>
+                <p>👤 <strong>Isaias Leos</strong>
+👤 <strong>Alex Vasquez</strong>
+👤 <strong>Jacob Padilla</strong>
+👤 <strong>Luis Soto</strong>
+👤 <strong>Andrew Clanan</strong></p>
+                <ul>
+                    <li>Github: <a href="https://github.com/IsaiasLeos">@IsaiasLeos</a></li>
+                    <li>Github: <a href="https://github.com/LXvsqz">@LXvsqz</a></li>
+                    <li>Github: <a href="https://github.com/JacobpaDILLAA">@JacobpaDILLAA</a></li>
+                    <li>Github: <a href="https://github.com/Luis9620">@Luis9620</a></li>
+                    <li>Github: <a href="https://github.com/aclanan">@aclanan</a></li>
+                </ul>
+
             </div>
         );
     }
