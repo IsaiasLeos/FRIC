21f279f7424955147fe84ddf18f467a568e94bcc
Luis Soto <lasotomorales@miners.utep.edu>
2020-10-12 17:29:17 -0600

    merge lasotomorales - ialeos

--- fric/src/findingscontentview/findingContentView.js
+++ fric/src/findingscontentview/findingContentView.js
@@ -17,172 +17,6 @@ function TaskContentView(){
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-<<<<<<< HEAD
-    return (
-        <div>
-            <GeneralView />
-            <div class="main">
-                <h2>Findings Overview Table</h2>
-                <Table bordered hover striped>
-                    <thead class="thead-grey">
-                        <tr>
-                            <th>Select</th>
-                            <th>ID<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Title<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>System<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Task<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Subtask<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Analyst<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Status<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Classification<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Type<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Risk<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="Finding1"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td>Title 1</td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding2"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td>Title 1</td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding3"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td>Title 1</td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding4"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td>Title 1</td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                    </tbody>
-                </Table>
-
-                <br></br>
-                <div className="title-buttons">
-                    <h2>Findings Overview Table</h2>
-                    <ButtonGroup>
-                        <Button variant="dark">Save</Button>
-                        <Button variant="dark">Delete</Button>
-                        <Button variant="dark">Cancel</Button>
-                        <input type="image" src={AddImage} onClick={handleShow} alt="Add button"/>
-                    </ButtonGroup>
-                </div>
-
-                <Table bordered hover striped>
-                    <thead class="thead-grey">
-                        <tr>
-                            <th>Select</th>
-                            <th>ID<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Title<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>System<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Task<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Subtask<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Analyst<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Status<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Classification<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Type<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                            <th>Risk<input type="image" src={SortImage} className="sort-button" alt="sort button" /></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td className="Finding1"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td><Button variant="outline-dark" onClick={handleShow}>Finding 1</Button></td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding2"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td><Button variant="outline-dark" onClick={handleShow}>Finding 2</Button></td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding3"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td><Button variant="outline-dark" onClick={handleShow}>Finding 3</Button></td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                        <tr>
-                            <td className="Finding4"><input type="checkbox" id="" name="" value="1"></input></td>
-                            <td>234</td>
-                            <td><Button variant="outline-dark" onClick={handleShow}>Finding 4</Button></td>
-                            <td>System W</td>
-                            <td>Task 1 and Task 2</td>
-                            <td>N/A</td>
-                            <td>Alex Vasquez</td>
-                            <td>Open</td>
-                            <td>Vulnerability</td>
-                            <td>Physical Security</td>
-                            <td>VL</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                    <Button variant="dark">Generate ERB Report</Button>
-                    &nbsp;
-                    <Button variant="dark">Generate Risk Matrix</Button>
-                    &nbsp;
-                    <Button variant="dark">Generate Final Report</Button>
-=======
     return(
             <div>
                 <GeneralView />
@@ -279,7 +113,6 @@ function TaskContentView(){
                         &nbsp;
                         <Button variant = "dark">Generate Final Report</Button>
                     
->>>>>>> origin/ialeos
 
                     <br></br>
                     <br></br>
@@ -312,16 +145,7 @@ function TaskContentView(){
                 <div class="right-tree">
                     <Tree />
                 </div>
-<<<<<<< HEAD
-           
-            <div class="right-tree">
-                    <Tree />
             </div>
-        </div>
-        </div>
-=======
-            </div>
->>>>>>> origin/ialeos
     );
 }
 
