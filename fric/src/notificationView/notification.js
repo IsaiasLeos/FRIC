1801727d807258b97d947ec82ae38d17ed721ab7
Jacob Padilla <jacobpadilla@Jacobs-MacBook-Pro.local>
2020-10-11 19:25:10 -0600

    Updated layout

---
+++ fric/src/notificationView/notification.js
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import 'react-bootstrap'
+import GeneralView from '../generalView/generalView';
+import '../assets/css/bootstrap.css'
+
+class setupContentView extends React.Component {
+    
+    render() {
+        
+        return (
+            <div>
+                <GeneralView />
+                <div className="Notify">
+
+                    <label for="taskTitle">
+                        Task Title:<br/>
+                        <input type="text" name="taskT" id="tTitle" className="T-title" placeholder="Task1" />
+                    </label><br />
+
+                    <label for="taskDueDate">
+                        Task Due Date:<br/>
+                        <input type="text" name="task-Due-Date" id="tDueDate" className="T-dueDate" placeholder="11/11/2020" />
+                    </label><br/>
+
+                    <label for="subtaskTitle">
+                        SubTask Title:<br/>
+                        <input type="text" name="subtaskT" id="sTitle" className="S-title" placeholder="Subtask3"/>
+                    </label><br />
+
+                    <label for="subtaskDueDate">
+                        SubTask Due Date:<br/>
+                        <input type="text" name="subtask-Due-Date" id="sDueDate" className="S-dueDate" placeholder="11/10/2020"/>
+                    </label><br/>
+
+                    
+
+                </div>    
+            </div>
+        );
+    }
+}
+
+
+export default notification;
