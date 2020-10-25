b15c577c69b43041920bc1272a13914ed1fcc84b
Jacob Padilla <jacobpadilla@Jacobs-MacBook-Pro.local>
2020-10-25 13:16:47 -0600

    fixed issues with notification

--- fric/src/generalView/generalView.js
+++ fric/src/generalView/generalView.js
@@ -3,63 +3,58 @@ import '../generalView/style.css'
 import 'react-bootstrap'
 import '../assets/css/bootstrap.css'
 import Modal from 'react-bootstrap/Modal'
-import {useState} from "react"
+import { useState } from "react"
 import Button from 'react-bootstrap/Button'
 function GeneralView() {
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false); // Close Modal View
-    const handleShow = () => setShow(true); // Open
-    function viewNote(){
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    function viewNote() {
         handleShow();
     }
     return (
         <div>
-            <nav class="navbar navbar-expand navbar-dark bg-dark">
-                <a class="navbar-brand" href="/Setup">FRIC</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02"
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <a className="navbar-brand" href="/Setup">FRIC</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02"
                     aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarsExample02">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link " href="/Event">Event</a>
-                            {/* <Link to="/Event" >Event </Link> */}
+                <div className="collapse navbar-collapse" id="navbarsExample02">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link " href="/Event">Event</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/SystemView">System</a>
-                            {/* <Link to="/SystemView" >System Content </Link> */}
+                        <li className="nav-item">
+                            <a className="nav-link" href="/SystemMaster">System</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/Findings">Findings</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Findings">Findings</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/Task">Task</a>
-                            {/* <Link to="/Task" >Task </Link> */}
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Task">Task</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/Subtask">Subtask</a>
+                        <li className="nav-item">
+                            <a className="nav-link " href="/Subtask">Subtask</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/Archive">Archive</a>
+                        <li className="nav-item">
+                            <a className="nav-link " href="/Archive">Archive</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/Configuration">Configuration</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Configuration">Configuration</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/Setup">Setup</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Setup">Setup</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/Help">Help</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/Help">Help</a>
                         </li>
                         <li class="nav-item">
                             <div class="nav-link" onClick={viewNote}> Notification </div>
                         </li>
-
-
                     </ul>
-                    <form class="form-inline my-2 my-md-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <form className="form-inline my-2 my-md-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
                     </form>
                 </div>
             </nav>
@@ -69,34 +64,33 @@ function GeneralView() {
                     <Modal.Title>Notification</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                <div style={{textAlign:"center"}}className="Notify">
-                    
-                    <label for="taskTitle">
-                        Task Title:<br/>
-                        <input type="text" name="taskT" value="Task1"/>
-                    </label><br/>
+                    <div style={{ textAlign: "center" }} className="Notify">
 
-                    <label for="taskDueDate">
-                        Task Due Date:<br/>
-                        <input type="text" name="task-Due-Date" value="11/11/2020" />
-                    </label><br/>
+                        <label for="taskTitle">
+                            Task Title:<br />
+                            <input type="text" name="taskT" value="Task1" />
+                        </label><br />
 
-                    <label for="subtaskTitle">
-                        SubTask Title:<br/>
-                        <input type="text" name="subtaskT" value="Subtask3"/>
-                    </label><br />
+                        <label for="taskDueDate">
+                            Task Due Date:<br />
+                            <input type="text" name="task-Due-Date" value="11/11/2020" />
+                        </label><br />
 
-                    <label for="subtaskDueDate">
-                        SubTask Due Date:<br/>
-                        <input type="text" name="subtask-Due-Date" value="11/10/2020"/>
-                    </label><br/>
-                </div>    
+                        <label for="subtaskTitle">
+                            SubTask Title:<br />
+                            <input type="text" name="subtaskT" value="Subtask3" />
+                        </label><br />
+
+                        <label for="subtaskDueDate">
+                            SubTask Due Date:<br />
+                            <input type="text" name="subtask-Due-Date" value="11/10/2020" />
+                        </label><br />
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" onClick={handleClose}> Okay! </Button>
                 </Modal.Footer>
             </Modal>
-
         </div>
     );
 }
