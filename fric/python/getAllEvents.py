d3eaa6fed529841707fadf5119add9404ce5787c
IsaiasLeos <blade_master_pantherinae@hotmail.com>
2020-10-26 17:36:38 -0600

    bugfixes for getting content

--- fric/python/getAllEvents.py
+++ fric/python/getAllEvents.py
@@ -57,21 +57,22 @@ def systems():
     mySystemCollection = mydb["system"]
     myFindingCollection = mydb["finding"]
     myTaskCollection = mydb["task"]
-    mySubtaskCollection = mydb["subtask"]
     system_json = []
     findings_json = []
     for f in myFindingCollection.find():
-        
-        findings_json.append({"findingID": f["Finding_ID"], "hostName": f["Host_Name"]})
+
+        findings_json.append(
+            {"findingID": f["Finding_ID"], "hostName": f["Host_Name"]})
         num_finds = len(findings_json)
     task_json = []
     for f in myTaskCollection.find():
-        
-        task_json.append({"taskTitle": f["Task_title"], "taskDescription": f["Task_Description"]})
+
+        task_json.append(
+            {"taskTitle": f["Task_title"], "taskDescription": f["Task_Description"]})
     num_tasks = len(task_json)
 
     for e in mySystemCollection.find():
-        
+
         task_json = []
 
         system_json.append({"sysInfo": e['System_Info'], "sysDesc": e['System_Description'], "sysLoc": e['System_Location'], "sysRouter": e['System_Router'], "sysSwitch": e['System_Switch'],  "sysRoom": e['System_Room'],
@@ -119,11 +120,13 @@ def addFinding():
     finding = {"Finding_ID": req['findingID'], "Host_Name": req['hostName']}
     mycollection.insert_one(finding)  # Send information to collection
 
+
 @app.route('/subtasks')
 def subtasks():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["FRIC"]
     mycollection = mydb["subtask"]
+    myFindingCollection = mydb["finding"]
     subtask_json = []
 
     findings_json = []
@@ -135,42 +138,44 @@ def subtasks():
 
     for e in mycollection.find():
         subtask_json.append({
-            "subtaskTitle": e['Subtask_Title'], 
+            "subtaskTitle": e['Subtask_Title'],
             "subtaskDescription": e['Subtask_Description'],
-            "subtaskProgress": e['Subtask_Progress'], 
-            "subtaskDueDate": e['Subtask_Due_Date'], 
-            "analysts": e['Analysts'],  
-            "collaborators": e['Collaborators'], 
-            "relatedTask": e['Related_Task'], 
-            "subtasks": e['Subtasks'], 
+            "subtaskProgress": e['Subtask_Progress'],
+            "subtaskDueDate": e['Subtask_Due_Date'],
+            "analysts": e['Analysts'],
+            "collaborators": e['Collaborators'],
+            "relatedTask": e['Related_Task'],
+            "subtasks": e['Subtasks'],
             "attachments": e['Attachments'],
             "numFindings": num_finds,
             "analyst": e['Analyst'],
             "task": e['Task']
-            })
+        })
     return jsonify(subtask_json)
 
-@app.route('/addsubtask',methods=['POST'])
+
+@app.route('/addsubtask', methods=['POST'])
 def addSubtasks():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["FRIC"]
     mycollection = mydb["subtask"]
     req = request.get_json()
     subtask = {
-        "Subtask_Title" : req['subtaskTitle'], 
-        "Subtask_Description" : req['subtaskDescription'], 
-        "Subtask_Progress" : req['subtaskProgress'], 
-        "Subtask_Due_Date" : req['subtaskDueDate'], 
-        "Analysts" : req['analysts'], 
-        "Collaborators" : req['collaborators'], 
-        "Related_Task" : req['relatedTask'], 
-        "Subtasks" : req['subtasks'], 
-        "Attachments" : req['attachments'],
-        "Num_Findings" : 0,
-        "Analyst" : "Analyst 0",
-        "Task" : "Task 0"
-        }
+        "Subtask_Title": req['subtaskTitle'],
+        "Subtask_Description": req['subtaskDescription'],
+        "Subtask_Progress": req['subtaskProgress'],
+        "Subtask_Due_Date": req['subtaskDueDate'],
+        "Analysts": req['analysts'],
+        "Collaborators": req['collaborators'],
+        "Related_Task": req['relatedTask'],
+        "Subtasks": req['subtasks'],
+        "Attachments": req['attachments'],
+        "Num_Findings": 0,
+        "Analyst": "Analyst 0",
+        "Task": "Task 0"
+    }
     mycollection.insert_one(subtask)
+
 
 @app.route('/tasks')
 def tasks():
@@ -191,49 +196,51 @@ def tasks():
     subtask_json = []
     # Get number of systems
     for s in mySubtaskCollection.find():
-        subtask_json.append({"subtaskTitle": s["Subtask_Title"], "subtaskDescription": s["Subtask_Description"]})
+        subtask_json.append(
+            {"subtaskTitle": s["Subtask_Title"], "subtaskDescription": s["Subtask_Description"]})
     num_subtask = len(subtask_json)
 
-
     for e in mycollection.find():
-        
+
         task_json.append({
-            "taskTitle": e['Task_title'], 
+            "taskTitle": e['Task_title'],
             "taskDescription": e['Task_Description'],
-            "system": e['System'], 
-            "taskPriority": e['Task_Priority'], 
-            "taskProgress": e['Task_Progress'],  
-            "taskDueDate": e['Task_Due_Date'], 
-            "taskAnalysts": e['Task_Analysts'], 
-            "taskCollaborators": e['Task_Collaborators'], 
-            "relatedTasks": e['Related_Tasks'], 
+            "system": e['System'],
+            "taskPriority": e['Task_Priority'],
+            "taskProgress": e['Task_Progress'],
+            "taskDueDate": e['Task_Due_Date'],
+            "taskAnalysts": e['Task_Analysts'],
+            "taskCollaborators": e['Task_Collaborators'],
+            "relatedTasks": e['Related_Tasks'],
             "attachments": e['Attachments'],
             "num_subtask": num_subtask,
             "num_finding": num_finds
-            })
+        })
     return jsonify(task_json)
 
-@app.route('/addtask',methods=['POST'])
+
+@app.route('/addtask', methods=['POST'])
 def addTasks():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["FRIC"]
     mycollection = mydb["task"]
     req = request.get_json()
     task = {
-        "Task_title" : req['taskTitle'], 
-        "Task_Description" : req['taskDescription'], 
-        "System" : req['system'], 
-        "Task_Priority" : req['taskPriority'], 
-        "Task_Progress" : req['taskProgress'], 
-        "Task_Due_Date" : req['taskDueDate'], 
-        "Task_Analysts" : req['taskAnalysts'], 
-        "Task_Collaborators" : req['taskCollaborators'], 
-        "Related_Tasks" : req['relatedTasks'], 
-        "Attachments" : req['attachments'],
-        "Num_subtask" : 0,
-        "Num_finding" : 13 
-        }
+        "Task_title": req['taskTitle'],
+        "Task_Description": req['taskDescription'],
+        "System": req['system'],
+        "Task_Priority": req['taskPriority'],
+        "Task_Progress": req['taskProgress'],
+        "Task_Due_Date": req['taskDueDate'],
+        "Task_Analysts": req['taskAnalysts'],
+        "Task_Collaborators": req['taskCollaborators'],
+        "Related_Tasks": req['relatedTasks'],
+        "Attachments": req['attachments'],
+        "Num_subtask": 0,
+        "Num_finding": 13
+    }
     mycollection.insert_one(task)
+
 
 @app.route('/addlog', methods=['POST'])
 def addLog():
