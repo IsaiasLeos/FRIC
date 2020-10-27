9b68c7411e4206f17ed5c83a884be9fead4218e6
Jacob Padilla <jacobpadilla@Jacobs-MacBook-Pro.local>
2020-10-26 19:31:57 -0600

    setup udated

--- fric/src/setupContentView/setupContentView.js
+++ fric/src/setupContentView/setupContentView.js
@@ -3,6 +3,7 @@ import 'react-bootstrap';
 import GeneralView from '../generalView/generalView';
 import '../assets/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from "react";
 
 function getCurrentDate(separator = '') {
   let newDate = new Date()
@@ -12,35 +13,27 @@ function getCurrentDate(separator = '') {
   return `${month < 10 ? `0${month}` : `${month}`}${separator}${day}${separator}${year}`
 }
 
-class setupContentView extends React.Component {
-  constructor() {
-    super();
-    this.state = { name: '', desc: '', type: '', vers: '', assess_date: '', org_name: '', event_class: '', declass_date: '', customer_name: '' };
-    this.action = { date: "", action: "", analyst: "" };
-  }
+function SetupContentView(props) {
 
-  handleEventType(e) {
-    console.log(e.target.value); // Get value from select tag // 
-  }
-  handleEventClass(e) {
-    console.log(e.target.value); // Get value from select tag // 
-  }
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-  onSubmit = (e) => {
+  const [event, setEvent] = useState([{
+    name: ''
+  }])
+
+  let action = { date: "", action: "", analyst: "" };
+  let state = { data: [], name: "", customer: "" };
+  function onSubmit(e) {
     e.preventDefault();
-    this.action.action = "submit event";//Logging information
-    this.action.date = getCurrentDate("/");
-    this.action.analyst = "";
-    this.props.history.push('/Event');//Used for submit button, will navigate to page and push to the DB
+    action.action = "submit event";//Logging information
+    action.date = getCurrentDate("/");
+    action.analyst = "";
+    props.history.push('/Event');//Used for submit button, will navigate to page and push to the DB
 
     fetch('/addlog', {//used to send logging information to DB
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(this.action),
+      body: JSON.stringify(action),
     }).then(response => response.json())
       .then(data => {
         console.log("Success", data);
@@ -54,7 +47,7 @@ class setupContentView extends React.Component {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(this.state),
+      body: JSON.stringify(state),
     }).then(response => response.json())
       .then(data => {
         console.log("Success", data);
@@ -64,48 +57,47 @@ class setupContentView extends React.Component {
       });
 
   }
-
-  checkEvent = (message) => {
+  function use() {
     fetch('/eventsOverview').then(
-      response => response.json()).then(data => this.setState(data))
-    console.log(this.state.name)
-    if (this.state.name.length > 0) {
-      //this.setState({message : true})
-      this.setState(message = <h4 style={{ textAlign: "center" }}>No Events Available </h4>)
-    }
-    else {
-
-      this.setState({ message: false })
-      //return message = <h4 style={{textAlign:"center"}}> Events Available </h4>
-      this.setState(message = <h4 style={{ textAlign: "center" }}>Events Available </h4>)
-    }
+      response => response.json()).then(data => setEvent({
+        data: data
+      })).catch(error => console.error(error));
   }
 
-  render() {
-    let message = this.checkEvent;
-    return (
+  function checker() {
+    console.log(event.data.length)
+    // if ((event.data === null)) {
+    //   return <h4 style={{ textAlign: 'center' }}>No event</h4>;
+    // }
+    // else {
+    //   return <h4 style={{ textAlign: 'center' }}>Event</h4>;
+    // }
+  }
+
+  return (
+    <div>
+      <GeneralView /><br />
+      {use()}
+      {checker()}
+      <h1 style={{ textAlign: "center" }}> Finding and Reporting Information Console (FRIC) </h1> <br />
       <div>
-        <GeneralView /><br />
-        <h1 style={{ textAlign: "center" }}> Finding and Reporting Information Console (FRIC) </h1> <br />
-        <div>
-          <h4 style={{ textAlign: "center" }}> Checking...</h4>
-          {message}
-        </div>
-        <form style={{ textAlign: "center" }} onSubmit={this.onSubmit} ><br /><br />
-          <label>
-            <h6>There is no existing event in your system </h6>
-            <input type="text" placeholder="e.g. Event1" name="name" onChange={this.onChange} id="event-title" className="event-data" />
-          </label> <br /><br />
-          <label>
-            <h6>Please enter your intials</h6>
-            <input type="text" placeholder="e.g. AC:" name="customer_name" onChange={this.onChange} id="event-customer-name" className="event-data" />
-          </label><br /><br />
-
-          <Button type="submit" className="btn" variant="outline-dark" >Submit</Button> <br /><br />
-        </form>
+        <h4 style={{ textAlign: "center" }}> Checking...</h4>
       </div>
-    );
-  }
+
+      <form style={{ textAlign: "center" }} onSubmit={onSubmit} ><br /><br />
+        <label>
+          <h6>There is no existing event in your system </h6>
+          <input type="text" placeholder="e.g. Event1" name="name" id="event-title" className="event-data" />
+        </label> <br /><br />
+        <label>
+          <h6>Please enter your intials</h6>
+          <input type="text" placeholder="e.g. AC:" name="customer_name" id="event-customer-name" className="event-data" />
+        </label><br /><br />
+
+        <Button type="submit" className="btn" variant="outline-dark" >Submit</Button> <br /><br />
+      </form>
+    </div>
+  );
 }
 
-export default setupContentView;
+export default SetupContentView;
