0e9035e2ccac3129c17e642bf7e24f4769d5fc6c
Luis Soto <lasotomorales@miners.utep.edu>
2020-11-03 16:31:04 -0700

    Fix button in subtask detailed view

--- fric/src/subtaskContentView/subtaskDetailedView.js
+++ fric/src/subtaskContentView/subtaskDetailedView.js
@@ -11,6 +11,7 @@ function getCurrentDate(separator = '') {
     let time = newDate.toTimeString()
     return `${month < 10 ? `0${month}` : `${month}`}${separator}${day}${separator}${year} - ${time}`
 }
+
 class subtaskDetailedView extends React.Component {
     constructor() {
         super();
